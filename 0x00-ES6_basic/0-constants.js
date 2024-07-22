74c3c9b85e3aaa729667ef0c7ae15704a5299aed
LovethLaw <talktoawi@gmail.com>
2024-07-22 15:43:05 +0100

    done

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable */
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
@@ -12,6 +10,5 @@ export function getLast() {
 export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-
     return combination;
 }
