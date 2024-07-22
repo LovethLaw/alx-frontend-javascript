39adfe96f1b6618630b5cb5a91143ba29f110ef2
LovethLaw <talktoawi@gmail.com>
2024-07-22 15:53:24 +0100

    check

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 
 export default function appendToEachArrayValue(array, appendString) {
-    for (const [idx] of array.entries()) {
-        const value = array[idx];
-        array[idx] = `${appendString}${value}`;
+    const newArray = []
+    for (let idx of array) {
+        newArray.push(appendString + idx);
     }
-
-    return array;
+    return newArray;
 }
