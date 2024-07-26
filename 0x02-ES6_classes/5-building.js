45ba528e5f98719a7b74daf3b8e8ead1c03838f6
LovethLaw <talktoawi@gmail.com>
2024-07-26 11:23:42 +0100

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,26 +1,15 @@
 /* eslint-disable */
 
-export default class Building {
-	constructor(sqft) {
-		if (new.target === Building) {
-			throw new TypeError('Cannot construct Building instances directly');
-		}
+export class Building {
+	constructor(sqft = 0) {
 
-		this.sqft = sqft;
+		if (this.constructor !== Building && typeof this.evacuationWarningMessage !== undefined) {
+			throw new Error("Class extending Building must override evacuationWarningMessage");
+		}
+		this._sqft = sqft;
 	}
 
 	get sqft() {
 		return this._sqft;
-	}
-
-	set sqft(value) {
-		if (value !== 'number') {
-			throw new Error("sqft must be a number");
-		}
-		this._sqft = value;
-	}
-
-	evacuationWarningMessage() {
-		throw new Error('Class extending Building must override evacuationWarningMessage');
 	}
 }
