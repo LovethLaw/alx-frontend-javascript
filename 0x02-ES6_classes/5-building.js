fb3d9d120e05eec78098d7b6041ed96e15e05153
LovethLaw <talktoawi@gmail.com>
2024-07-26 11:30:16 +0100

    task done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,18 @@
 /* eslint-disable */
 
 export class Building {
-	constructor(sqft = 0) {
+	constructor(sqft) {
 
 		if (this.constructor !== Building && typeof this.evacuationWarningMessage !== undefined) {
 			throw new Error("Class extending Building must override evacuationWarningMessage");
 		}
-		this._sqft = sqft;
+		this.sqft = sqft;
 	}
 
 	get sqft() {
 		return this._sqft;
+	}
+	set sqft(value) {
+		this._sqft = value;
 	}
 }
